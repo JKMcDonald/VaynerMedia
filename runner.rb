809d1c0275151987371766e010f72711d3e8a295
Kyle McDonald <kyle2mcdonald@gmail.com>
2017-08-03 21:44:59 -0400

    working on question 2

--- runner.rb
+++ runner.rb
@@ -1,5 +1,6 @@
 require_relative 'data'
 require_relative 'data_parser'
+require 'pry'
 
 
 if ARGV.any?
@@ -7,29 +8,40 @@ if ARGV.any?
   data1 = DataParser.parse('source1.csv')
   data2 = DataParser.parse('source2.csv')
 
+#make a new array of the two parsed CSV files where the corresponding campaign_id's group them into nested arrays
 combined_data = []
   data1.each do |dataset1|
     data2.each do |dataset2|
       if dataset1.campaign_id == dataset2.campaign_id
-        combined_data << dataset1.inject_instance_variables(dataset2)
-      else
-        combined_data << dataset1
-        combined_data << dataset2
+        combined_data << [dataset1, dataset2]
       end
     end
   end
-p combined_data
+
 
 command = ARGV.first
 options = ARGV[1..-1]
 
-
+#Case conditional where using ARGV to ask a question and prompt a certain response
 case command
 when "purple"
   total = []
-  purple_hair_spend = combined_data.select {|person| total << person.has_hair_color?(command)}
+  combined_data.select do |person| 
+    if person[0].has_hair_color(command)
+      total << person[1].spend.to_i
+    end
+  end
   puts "The total amount spent toward people with #{command} hair"
-  purple_hair_spend.reduce(:+)
+  p total.reduce(:+)
+when "4+ days"
+  counter = 0
+    data2.each do |row| 
+      binding.pry
+    if data2.count(row.campaign_id) > 4
+      counter +=1
+    end
+  end
+  puts "#{counter} campaigns spent on 4 + days"
 end
 end
   
